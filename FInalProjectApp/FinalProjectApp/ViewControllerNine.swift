cc9b5daf722c75c14a2d21b855eb50a7e6b55159
Ioana <33571434+ioananed@users.noreply.github.com>
2021-08-05 18:18:07 +0200

    fixedAllTheErrors

--- FInalProjectApp/FinalProjectApp/ViewControllerNine.swift
+++ FInalProjectApp/FinalProjectApp/ViewControllerNine.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewControllerNine: UIViewController {
 
-    @IBOutlet weak var questionLabelVC9: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +16,6 @@ class ViewControllerNine: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func dataSciencePressed(_ sender: UIButton) {
-    }
-    
-    @IBAction func softwareDevPressed(_ sender: UIButton) {
-    }
-    
-    @IBAction func programmingPressed(_ sender: UIButton) {
-    }
     
     /*
     // MARK: - Navigation
